d410466752352c7a4636d678ab572850f68a600e
jakiemarkar <81985147+jakiemarkar@users.noreply.github.com>
2022-04-14 20:15:43 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,7 @@
 local gameid = game.PlaceId
+if gameid == nil then
+  game.Players.LocalPlayer:Kick("Please Rejoin")
+end
 if gameid == 1537690962 then
   loadstring(game:HttpGet(('https://raw.githubusercontent.com/jakiemarkar/NeNec/main/BeeSwarm.lua')))()
 elseif gameid == 6679274937 then
