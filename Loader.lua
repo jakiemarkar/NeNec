f1a610baf5e5d2a69a9b5e116c3069a66e5bec77
jakiemarkar <81985147+jakiemarkar@users.noreply.github.com>
2022-04-14 17:49:40 +0200

    Update and rename Loader to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,6 @@
+local gameid = game.PlaceId
+if gameid == 1537690962 then
+  loadstring(game:HttpGet(('https://raw.githubusercontent.com/jakiemarkar/NeNec/main/BeeSwarm.lua')))()
+elseif gameid == 6679274937 then
+  loadstring(game:HttpGet(('https://raw.githubusercontent.com/jakiemarkar/NeNec/main/Vans.lua')))()
+end
